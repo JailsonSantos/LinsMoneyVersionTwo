528d7b1ec2928d4bcc4ffb4ed4808df65e5ad342
JailsonSantos <jailson.ads007@gmail.com>
2022-08-10 12:12:14 -0300

    Estrutura visual: Tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,38 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsTable, TransctionsContainer } from "./styles";
 
 export function Transctions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransctionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento  de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/08/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Pizza</td>
+              <td>
+                <PriceHighlight variant="outcome"> - R$ 60,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>13/07/2022</td>
+            </tr>
+
+          </tbody>
+        </TransactionsTable>
+      </TransctionsContainer>
+
     </div>
   )
 }
